90c9fc27c98a9e692cd7695f70871ac975064723
Noelle Rolwes <nrolwes@gmail.com>
2018-09-07 21:46:52 +0000

    automatically backed up by learn

--- lib/math.rb
+++ lib/math.rb
@@ -11,15 +11,17 @@ def division(num1, num2)
 end
 
 def multiplication(num1, num2)
-  num
+  num1*num2
 end
 
 def modulo(num1, num2)
+  num1%num2
 end
 
 def square_root(num)
+  Math.sqrt(num)
 end
 
 def order_of_operation(num1, num2, num3, num4)
-  #Hint:  __ + (( __ * __ ) / __ )
+  num1 + (( num2 * num3 ) / num4 )
 end
